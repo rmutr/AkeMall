f3893a6f73b3bf2c82431bd11d5732096ce6fb6e
Sanya Samaimak <sanya.sam@rmutr.ac.th>
2019-11-26 15:57:24 +0700

    Read Firestore Success

--- lib/widget/show_list.dart
+++ lib/widget/show_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ShowList extends StatefulWidget {
@@ -6,6 +7,40 @@ class ShowList extends StatefulWidget {
 }
 
 class _ShowListState extends State<ShowList> {
+
+  // Field 
+  List<String> names = List();
+  List<String> details = List();
+  List<String> images = List();
+  List<String> codes = List();
+
+  // Method 
+  @override
+  void initState(){
+    super.initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore()async {
+
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Product');
+    await collectionReference.snapshots().listen((QuerySnapshot querySanpshot){
+      List<DocumentSnapshot> snapshots = querySanpshot.documents; 
+
+      for (var snapshot in snapshots) {
+        names.add(snapshot.data['Name']);
+        details.add(snapshot.data['Detail']);
+        images.add(snapshot.data['Image']);
+        codes.add(snapshot.data['Code']);
+
+        print('names[0] = ${names[0]}');
+      }
+
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
